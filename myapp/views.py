b6b6ee31185853b0c951687510ecc5f371ac4c37
Jyothi0317 <you@example.com>
2020-08-13 18:30:44 +0530

    third commit

--- myapp/views.py
+++ myapp/views.py
@@ -57,11 +57,16 @@ def multi(request):
 from django.core.files.storage import FileSystemStorage
 
 def img_upld(request):
+     return render(request,"img_upld.html")
+    
+
+def img_display(request):
     file_url=False
     if request.method=="POST" and request.FILES:
         image=request.FILES['sam']
+        print(image)
         fs=FileSystemStorage()
         file=fs.save(image.name,image)
         file_url=fs.url(file)
                 
-    return render(request,"img_upld.html",context={'file_url':file_url})
+    return render(request,"img_display.html",context={'file_url':file_url})
